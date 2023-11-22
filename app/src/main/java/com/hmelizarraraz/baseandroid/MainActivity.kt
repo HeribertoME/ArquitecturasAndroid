1e1693509af3ded26ff28d2963911201476f74a9
Heriberto Martinez Elizarraraz <heriberto.martineze-ext@ab-inbev.com>
2023-11-21 22:45:08 -0600

    View Model is added
    Se agrega view model y se refactoriza la vista

--- app/src/main/java/com/hmelizarraraz/baseandroid/MainActivity.kt
+++ app/src/main/java/com/hmelizarraraz/baseandroid/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
@@ -14,7 +17,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -22,10 +29,16 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.hmelizarraraz.baseandroid.ui.theme.ArquitecturasAndroidTheme
 import retrofit2.Retrofit
@@ -33,21 +46,24 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 // 36085a2beed6820970b1a47e9bed8b67
 class MainActivity : ComponentActivity() {
+
+    // En XML vistas clasicas
+    // val viewModel: MainViewModel by viewModels()
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ArquitecturasAndroidTheme {
 
-                val movies = produceState<List<Servermovie>>(initialValue = emptyList()) {
-                    value = Retrofit.Builder()
-                        .baseUrl("https://api.themoviedb.org/3/")
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build()
-                        .create(MoviesService::class.java)
-                        .getMovies()
-                        .results
-                }
+                val viewModel: MainViewModel = viewModel() // Para compose
+
+                // Con livedata y compose se necesita lo siguiente
+                //val state by viewModel.state.observeAsState(MainViewModel.UiState())
+                // Con activity basta usar el observer
+
+                // Para state flow y compose
+                val state by viewModel.state.collectAsState()
+                // Para activity hacer collect en activity
 
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -57,18 +73,30 @@ class MainActivity : ComponentActivity() {
                     Scaffold(
                         topBar = { TopAppBar(title = { Text(text = "Movies") }) }
                     ) { padding ->
-                        LazyVerticalGrid(
-                            columns = GridCells.Adaptive(180.dp),
-                            modifier = Modifier.padding(padding),
-                            horizontalArrangement = Arrangement.spacedBy(4.dp),
-                            verticalArrangement = Arrangement.spacedBy(4.dp),
-                            contentPadding = PaddingValues(4.dp)
-                        ) {
-                            items(movies.value) { movie ->
-                                MovieItem(movie)
+                        if (state.loading) {
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                CircularProgressIndicator()
                             }
                         }
-                        
+                        if (state.movies.isNotEmpty()) {
+                            LazyVerticalGrid(
+                                columns = GridCells.Adaptive(120.dp),
+                                modifier = Modifier.padding(padding),
+                                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                                verticalArrangement = Arrangement.spacedBy(4.dp),
+                                contentPadding = PaddingValues(4.dp)
+                            ) {
+                                items(state.movies) { movie ->
+                                    MovieItem(
+                                        movie = movie,
+                                        onClick = { viewModel.onMovieClick(movie) }
+                                    )
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -77,15 +105,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MovieItem(movie: Servermovie) {
-    Column {
-        AsyncImage(
-            model = "https://image.tmdb.org/t/p/w185/${movie.poster_path}",
-            contentDescription = movie.title,
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(2 / 3f)
-        )
+fun MovieItem(movie: Servermovie, onClick: () -> Unit) {
+    Column(
+        modifier = Modifier.clickable(onClick = onClick)
+    ) {
+        Box {
+            AsyncImage(
+                model = "https://image.tmdb.org/t/p/w185/${movie.poster_path}",
+                contentDescription = movie.title,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(2 / 3f)
+            )
+            if (movie.favorite) {
+                Icon(
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = movie.title,
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(8.dp),
+                    tint = Color.Red
+                )
+            }
+        }
         Text(
             text = movie.title,
             modifier = Modifier.padding(16.dp),
--- app/src/main/java/com/hmelizarraraz/baseandroid/Servermovie.kt
+++ app/src/main/java/com/hmelizarraraz/baseandroid/Servermovie.kt
@@ -14,5 +14,6 @@ data class Servermovie(
     val title: String,
     val video: Boolean,
     val vote_average: Double,
-    val vote_count: Int
+    val vote_count: Int,
+    val favorite: Boolean = false
 )
